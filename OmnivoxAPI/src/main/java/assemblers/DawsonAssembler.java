483179095d502cb16c2604a844d7c4b0b8e4a6ba
Defalt-Dev <felix.egan@icloud.com>
2024-09-23 14:46:41 -0400

    added temp imports and copycode

---
+++ OmnivoxAPI/src/main/java/assemblers/DawsonAssembler.java
@@ -0,0 +1,22 @@
+package assemblers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import com.gargoylesoftware.htmlunit.html.DomText;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import courses.CalendarEvent;
+import courses.CourseAssignment;
+import courses.CourseDocument;
+
+public class DawsonAssembler extends Assembler {
+
+    
+}
---
+++ OmnivoxAPI/src/main/java/scrapers/DawsonScraper.java
@@ -0,0 +1,89 @@
+package scrapers;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class DawsonScraper extends OmnivoxScraper {
+
+    	/**
+	 * The Omnivox login link for Maisonneuve college.
+	 */
+    private static final String loginUrl = "https://dawsoncollege.omnivox.ca/Login/Account/Login?ReturnUrl=%2fintr%2f";
+
+    public DawsonScraper() {
+        super(loginUrl);
+    }
+
+	@Override
+	public HtmlPage[] getDocumentPages() {
+		List<HtmlElement> classes = this.LeaPage.getByXPath("//*[@class='card-panel section-spacing']");
+		
+		// Creating return array
+		HtmlPage[] return_array = new HtmlPage[classes.size()];
+		
+		for(int i = 0; i < classes.size(); i++) {
+			
+			HtmlElement button = classes.get(i).getFirstByXPath("./div[2]/a");
+			
+			try {
+				return_array[i] = button.click();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return return_array;
+	}
+
+	@Override
+	public HtmlPage[] getAssignmentPages() {
+		List<HtmlElement> classes = this.LeaPage.getByXPath("//*[@class='card-panel section-spacing']");
+		
+		// Creating return array
+		HtmlPage[] return_array = new HtmlPage[classes.size()];
+		
+		for(int i = 0; i < classes.size(); i++) {
+			
+			HtmlElement button = classes.get(i).getFirstByXPath("./div[2]/a[2]");
+			
+			try {
+				return_array[i] = button.click();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return return_array;
+	}
+
+	@Override
+	public void printWhatsNew() {
+//		Check if there is anything new
+		List<HtmlElement> whats_new = this.homePage.<HtmlElement>getByXPath("//*[@id=\"qdn-sans-bouton-wrapper\"]/a/div[2]");
+		
+		if(whats_new.size() > 0) {
+			
+//			If there is something new
+//			Printing the text of the elements
+			for(HtmlElement news : whats_new) {
+				System.out.println(news.asText());
+			}
+		} else {
+			System.out.println("Nothing New");
+		}
+	}
+
+	@Override
+	public void setLeaPage() {
+		try {
+			this.LeaPage = this.homePage.<HtmlElement>getFirstByXPath("//*[@id='region-raccourcis-services-skytech']/a[1]").click();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
